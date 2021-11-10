8e7c832bc2159beeca89f31dd05b92416617e277
Quentin ROULET <quentin.roulet1@gmail.com>
2021-11-10 21:27:32 +0100

    addMenu

--- src/screens/CategoryScreen.jsx
+++ src/screens/CategoryScreen.jsx
@@ -1,12 +1,45 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
 export default function CategoryScreen(props) {
   const { categories } = props.route.params;
 
   const categoriesJsx = categories
     .sort((c1, c2) => c1.name.localeCompare(c2.name))
-    .map((c) => <Text key={c.id}>{c.name}</Text>);
+    .map((c) => <Picker.Item key={c.id} label={c.name} value={c.name} />);
 
-  return <View>{categoriesJsx}</View>;
+  const [category, setCategory] = useState("Unknown");
+
+  return (
+    <View style={styles.menu}>
+      <Picker
+        selectedValue={category}
+        onValueChange={(value, index) => setCategory(value)}
+        mode="dropdown"
+        style={styles.picker}
+      >
+        <Picker.Item label="Choisir une catégorie" value="Unknown" />
+        {categoriesJsx}
+      </Picker>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  menu: {
+    height: "100%",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  choice: {
+    backgroundColor: "red",
+  },
+  picker: {
+    marginVertical: 30,
+    width: 300,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#666",
+  },
+});
--- src/screens/LogicielScreen.jsx
+++ src/screens/LogicielScreen.jsx
@@ -1,12 +1,45 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
 export default function CategoryScreen(props) {
   const { software } = props.route.params;
 
   const softwareJsx = software
     .sort((c1, c2) => c1.name.localeCompare(c2.name))
-    .map((s) => <Text key={s.id}>{s.name}</Text>);
+    .map((s) => <Picker.Item key={s.id} label={s.name} value={s.name} />);
 
-  return <View>{softwareJsx}</View>;
+  const [soft, setSoft] = useState("Unknown");
+
+  return (
+    <View style={styles.menu}>
+      <Picker
+        selectedValue={soft}
+        onValueChange={(value, index) => setSoft(value)}
+        mode="dropdown"
+        style={styles.picker}
+      >
+        <Picker.Item label="Choisir une catégorie" value="Unknown" />
+        {softwareJsx}
+      </Picker>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  menu: {
+    height: "100%",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  choice: {
+    backgroundColor: "red",
+  },
+  picker: {
+    marginVertical: 30,
+    width: 300,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#666",
+  },
+});
